9ab310a4b60407464085ea35c3871d18be6c23ee
Pouya Larjani <plj@fb.com>
2017-12-20 02:30:24 -0800

    2-Body gravity

--- src/model/Body.ts
+++ src/model/Body.ts
@@ -3,13 +3,17 @@ import { Vector3 } from 'three';
 import force from '../utils/force';
 
 export default class Body {
+  // Display name of this body
+  public name: String;
+
   // Parent element.Bodies with no parent use absolute coordinates
   private _parent?: Body;
 
   // Main axis relative to parent axis, normalized, defined "up"
   private _axis = new Vector3(0, 1, 0);
-  private _axisNormal = new Vector3();
-  private _axisAngle = 0.0;
+  protected axisAbsolute = new Vector3(0, 1, 0);
+  protected axisNormal = new Vector3();
+  protected axisAngle = 0.0;
 
   // Central position relative to parent's position and axis, [m^3]
   public position = new Vector3();
@@ -27,16 +31,17 @@ export default class Body {
   public radius = 0;
 
   // Bodies under sphere of influence of this body
-  readonly children = new Set<Body>();
+  public readonly children = new Set<Body>();
 
   constructor(
-    // Display name of this body
-    public name: String,
     // Unique identifier for this body
     public readonly id = Math.random().toString(36).substr(2, 9),
     // Last time vectors were calculated, [s]
     protected lastUpdated: number = Date.now() / 1000.0,
   ) {
+    if (!this.name) {
+      this.name = id;
+    }
   }
 
   get parent() {
@@ -48,10 +53,14 @@ export default class Body {
       this._parent.children.delete(this);
     }
 
-    this._parent = parent;
+    if (parent && (parent != this)) {
+      this._parent = parent;
+    } else {
+      this._parent = undefined;
+    }
 
-    if (parent) {
-      parent.children.add(this);
+    if (this._parent) {
+      this._parent.children.add(this);
     }
 
     // TODO: Calculate new axis vector
@@ -62,33 +71,41 @@ export default class Body {
   }
 
   set axis(axis: Vector3) {
-    const up = new Vector3(0, 1, 0);
     this._axis = axis.clone().normalize();
-    this._axisNormal.crossVectors(up, this._axis).normalize();
-    this._axisAngle = up.angleTo(this._axis);
+    this.axisAbsolute = this._axis.clone();
+    if (this._parent) {
+      this.axisAbsolute = this.axisAbsolute.applyAxisAngle(this._parent.axisNormal, this._parent.axisAngle);
+    }
+
+    const up = new Vector3(0, 1, 0);
+    this.axisNormal.crossVectors(up, this.axisAbsolute).normalize();
+    this.axisAngle = up.angleTo(this.axisAbsolute);
   }
 
-  getAbsolutePosition(offset?: Vector3): Vector3 {
-    let pos = offset
-      ? offset.applyAxisAngle(this._axisNormal, this._axisAngle).add(this.position)
-      : this.position.clone();
+  getAbsolutePosition(): Vector3 {
+    let pos = this.position.clone();
 
-    return this._parent
-      ? this._parent.getAbsolutePosition(pos)
-      : pos;
+    if (this._parent) {
+      pos = pos
+        .applyAxisAngle(this._parent.axisNormal, this._parent.axisAngle)
+        .add(this._parent.getAbsolutePosition());
+    }
+
+    return pos;
   }
 
   static readonly SimulationLevel = {
-    NoGravity: 0,
-    TwoBody: 1,
-    NBody: 2,
-    AllBodies: 3,
+    NoGravity:  0, // Use only velocity vector
+    TwoBody:    1, // + Gravity of parent
+    ThreeBody:  3, // + Gravity of grandparent
+    NBody:      4, // + Gravity of children
+    AllBodies:  5, // + Gravity of siblings and uncles
   }
 
   simulate(now: number, level: number) {
     // Find the delta-t and whether it's been long enough to simulate
     const dt = now - this.lastUpdated;
-    if (Math.abs(dt) < 0.01) {
+    if (Math.abs(dt) < 0.001) {
       return;
     }
     this.lastUpdated = now;
@@ -98,17 +115,10 @@ export default class Body {
     let f = this.velocity.clone().setLength(m);
 
     // Gravity of parent
-    /*
-    if (level >= Body.SimulationLevel.TwoBody && this.parent) {
-      let g = this.position.toSpherical(true);
-      g.rho = force.gravity(this.parent.mass, this.mass, g.rho);
-      f = f.add(g);
+    if (level >= Body.SimulationLevel.TwoBody && this._parent) {
+      let g = force.gravity(this._parent.mass, this.mass, this.position.length());
+      f = f.add(this.position.clone().negate().setLength(g));
     }
-    */
-
-    // Gravity of siblings and children
-
-    // Gravity at grandparent level
 
     // Apply the motion
     f = f.setLength(force.apply(f.length(), this.mass, dt));
--- src/model/__tests__/BodyTest.ts
+++ src/model/__tests__/BodyTest.ts
@@ -5,21 +5,24 @@ import { Vector3 } from 'three';
 import Body from '../Body';
 
 const pi = Math.PI;
+const expectVector =
+  (vec: Vector3, x: number, y: number, z: number) =>
+    expect(vec.clone().round()).to.deep.equal(new Vector3(x, y, z).round());
 
-describe('Basic body placement', () => {
+describe('Body placement', () => {
   let body1: Body;
   let body2: Body;
   let body3: Body;
 
   beforeEach('Setup bodies', () => {
-    body1 = new Body('1', '1', 0);
+    body1 = new Body('1', 0);
 
-    body2 = new Body('2', '2', 0);
+    body2 = new Body('2', 0);
     body2.parent = body1;
     body2.axis = new Vector3(0, 0, 1); // axis is facing the camera on z
     body2.position = new Vector3(1e+6, 0, 0); // 1000 km away on x
 
-    body3 = new Body('3', '3', 0);
+    body3 = new Body('3', 0);
     body3.parent = body2;
     body3.axis = new Vector3(0, 1, 0); // Facing normal "up" relative to parent
     body3.position = new Vector3(0, 10, 0); // 10 m away on y
@@ -32,160 +35,129 @@ describe('Basic body placement', () => {
   });
 
   it('level-0 absolute positioning', () => {
-    expect(body1.getAbsolutePosition()).to.deep.equal(new Vector3(0, 0, 0));
+    expectVector(body1.getAbsolutePosition(), 0, 0, 0);
     body2.parent = undefined;
-    expect(body2.getAbsolutePosition()).to.deep.equal(new Vector3(1e+6, 0, 0));
+    expectVector(body2.getAbsolutePosition(), 1e+6, 0, 0);
     body3.parent = undefined;
-    expect(body3.getAbsolutePosition()).to.deep.equal(new Vector3(0, 10, 0));
+    expectVector(body3.getAbsolutePosition(), 0, 10, 0);
   });
 
   it('multi-level absolute positioning', () => {
-    expect(body1.getAbsolutePosition()).to.deep.equal(new Vector3(0, 0, 0));
-    expect(body2.getAbsolutePosition()).to.deep.equal(new Vector3(1e+6, 0, 0));
-    expect(body3.getAbsolutePosition().round()).to.deep.equal(new Vector3(1e+6, 0, 10));
+    expectVector(body1.getAbsolutePosition(), 0, 0, 0);
+    expectVector(body2.getAbsolutePosition(), 1e+6, 0, 0);
+    expectVector(body3.getAbsolutePosition(), 1e+6, 0, 10);
   });
 
   it('axis tilt', () => {
     body1.position = new Vector3(1, 2, 3);
     body2.position = new Vector3(9, 8, 7);
-    expect(body1.getAbsolutePosition()).to.deep.equal(new Vector3(1, 2, 3));
-    expect(body2.getAbsolutePosition()).to.deep.equal(new Vector3(10, 10, 10));
+    expectVector(body1.getAbsolutePosition(), 1, 2, 3);
+    expectVector(body2.getAbsolutePosition(), 10, 10, 10);
 
     body2.axis = new Vector3(0, 1, 0);
-    expect(body3.getAbsolutePosition()).to.deep.equal(new Vector3(10, 20, 10));
+    expectVector(body3.getAbsolutePosition(), 10, 20, 10);
 
     body2.axis = new Vector3(1, 1, 1);
-    expect(body3.getAbsolutePosition().round()).to.deep.equal(new Vector3(16, 16, 16));
+    expectVector(body3.getAbsolutePosition(), 16, 16, 16);
   });
 
   it('multi axis tilt', () => {
     body1.axis = new Vector3(1, 1, 0);
     body2.axis = new Vector3(-1, 1, 0);
-    expect(body1.getAbsolutePosition()).to.deep.equal(new Vector3(0, 0, 0));
+    expectVector(body1.getAbsolutePosition(), 0, 0, 0);
 
     body2.position = new Vector3(0, 10, 0);
-    expect(body2.getAbsolutePosition().round()).to.deep.equal(new Vector3(7, 7, 0));
+    expectVector(body2.getAbsolutePosition(), 7, 7, 0);
 
     body2.position = new Vector3(0, 0, 10);
-    expect(body2.getAbsolutePosition().round()).to.deep.equal(new Vector3(0, 0, 10));
+    expectVector(body2.getAbsolutePosition(), 0, 0, 10);
 
     body2.position = new Vector3(10, 0, 0);
-    expect(body2.getAbsolutePosition().round()).to.deep.equal(new Vector3(7, -7, 0));
+    expectVector(body2.getAbsolutePosition(), 7, -7, 0);
 
-    expect(body3.getAbsolutePosition().round()).to.deep.equal(new Vector3(7, 3, 0));
+    expectVector(body3.getAbsolutePosition(), 7, 3, 0);
   });
 });
 
-describe('Basic body motion', () => {
+describe('Body motion', () => {
   let body1: Body;
   let body2: Body;
   let body3: Body;
 
   beforeEach('Setup bodies', () => {
-    body1 = new Body('1', '1', 0);
-    body1.mass = 1e+9;    // A million tons
-    body1.radius = 1e+3;  // 1 km
+    body1 = new Body('1', 0);
+    body1.mass = 1e6;
 
-    body2 = new Body('2', '2', 0);
+    body2 = new Body('2', 0);
     body2.parent = body1;
-    body2.mass = 1e+3; // 1000 kg
-    body2.radius = 1;  // 1 m
+    body2.mass = 1e3;
     body2.axis = new Vector3(0, 0, 1); // axis is facing the camera on z
     body2.position = new Vector3(1e+6, 0, 0); // 1000 km away on x
     body2.velocity = new Vector3(0, 1, 0); // moving up on y at 1 m/s
 
-    body3 = new Body('3', '3', 0);
+    body3 = new Body('3', 0);
     body3.parent = body2;
-    body3.mass = 1; // 1 kg
-    body3.radius = 0.01;  // 1 cm
+    body3.mass = pi;
     body3.axis = new Vector3(0, 1, 0); // Facing normal "up" relative to parent
     body3.position = new Vector3(0, 10, 0); // 10 m away on y
     body3.velocity = new Vector3(0, -5, 0); // moving down on y at 5 m/s
   });
 
   it('non-moving objects', () => {
-    expect(body1.getAbsolutePosition().round())
-      .to.deep.equal(new Vector3(0, 0, 0));
+    expectVector(body1.getAbsolutePosition(), 0, 0, 0);
 
     body1.simulate(0, Body.SimulationLevel.NoGravity);
-    expect(body1.getAbsolutePosition().round())
-      .to.deep.equal(new Vector3(0, 0, 0));
-    expect(body1.velocity.round())
-      .to.deep.equal(new Vector3(0, 0, 0));
+    expectVector(body1.getAbsolutePosition(), 0, 0, 0);
+    expectVector(body1.velocity, 0, 0, 0);
 
     body1.simulate(1, Body.SimulationLevel.NoGravity);
-    expect(body1.getAbsolutePosition().round())
-      .to.deep.equal(new Vector3(0, 0, 0));
-    expect(body1.velocity.round())
-      .to.deep.equal(new Vector3(0, 0, 0));
+    expectVector(body1.getAbsolutePosition(), 0, 0, 0);
+    expectVector(body1.velocity, 0, 0, 0);
 
     body1.simulate(1000, Body.SimulationLevel.NoGravity);
-    expect(body1.getAbsolutePosition().round())
-      .to.deep.equal(new Vector3(0, 0, 0));
-    expect(body1.velocity.round())
-      .to.deep.equal(new Vector3(0, 0, 0));
+    expectVector(body1.getAbsolutePosition(), 0, 0, 0);
+    expectVector(body1.velocity, 0, 0, 0);
   });
 
   it('no movement', () => {
-    expect(body2.getAbsolutePosition().round())
-      .to.deep.equal(new Vector3(1000000, 0, 0));
-    expect(body2.position.round())
-      .to.deep.equal(new Vector3(1000000, 0, 0));
-    expect(body2.velocity.round())
-      .to.deep.equal(new Vector3(0, 1, 0));
+    expectVector(body2.getAbsolutePosition(), 1000000, 0, 0);
+    expectVector(body2.position, 1000000, 0, 0);
+    expectVector(body2.velocity, 0, 1, 0);
 
     body2.simulate(0, Body.SimulationLevel.NoGravity);
-    expect(body2.getAbsolutePosition().round())
-      .to.deep.equal(new Vector3(1000000, 0, 0));
-    expect(body2.position.round())
-      .to.deep.equal(new Vector3(1000000, 0, 0));
-    expect(body2.velocity.round())
-      .to.deep.equal(new Vector3(0, 1, 0));
+    expectVector(body2.getAbsolutePosition(), 1000000, 0, 0);
+    expectVector(body2.position, 1000000, 0, 0);
+    expectVector(body2.velocity, 0, 1, 0);
 
     body2.simulate(0.0001, Body.SimulationLevel.NoGravity);
-    expect(body2.getAbsolutePosition().round())
-      .to.deep.equal(new Vector3(1000000, 0, 0));
-    expect(body2.position.round())
-      .to.deep.equal(new Vector3(1000000, 0, 0));
-    expect(body2.velocity.round())
-      .to.deep.equal(new Vector3(0, 1, 0));
+    expectVector(body2.getAbsolutePosition(), 1000000, 0, 0);
+    expectVector(body2.position, 1000000, 0, 0);
+    expectVector(body2.velocity, 0, 1, 0);
   });
 
   it('simple movement', () => {
     body2.simulate(1, Body.SimulationLevel.NoGravity);
 
-    expect(body2.getAbsolutePosition().round())
-      .to.deep.equal(new Vector3(1000000, 1, 0));
-    expect(body2.position.round())
-      .to.deep.equal(new Vector3(1000000, 1, 0));
-    expect(body2.velocity.round())
-      .to.deep.equal(new Vector3(0, 1, 0));
+    expectVector(body2.getAbsolutePosition(), 1000000, 1, 0);
+    expectVector(body2.position, 1000000, 1, 0);
+    expectVector(body2.velocity, 0, 1, 0);
   });
 
   it('stepwise movement', () => {
     body2.simulate(0.1, Body.SimulationLevel.NoGravity);
-    expect(body2.getAbsolutePosition().round())
-      .to.deep.equal(new Vector3(1000000, 0, 0));
-    expect(body2.position.round())
-      .to.deep.equal(new Vector3(1000000, 0, 0));
-    expect(body2.velocity.round())
-      .to.deep.equal(new Vector3(0, 1, 0));
+    expectVector(body2.getAbsolutePosition(), 1000000, 0, 0);
+    expectVector(body2.position, 1000000, 0, 0);
+    expectVector(body2.velocity, 0, 1, 0);
 
     body2.simulate(0.9, Body.SimulationLevel.NoGravity);
-    expect(body2.getAbsolutePosition().round())
-      .to.deep.equal(new Vector3(1000000, 1, 0));
-    expect(body2.position.round())
-      .to.deep.equal(new Vector3(1000000, 1, 0));
-    expect(body2.velocity.round())
-      .to.deep.equal(new Vector3(0, 1, 0));
+    expectVector(body2.getAbsolutePosition(), 1000000, 1, 0);
+    expectVector(body2.position, 1000000, 1, 0);
+    expectVector(body2.velocity, 0, 1, 0);
 
     body2.simulate(10, Body.SimulationLevel.NoGravity);
-    expect(body2.getAbsolutePosition().round())
-      .to.deep.equal(new Vector3(1000000, 10, 0));
-    expect(body2.position.round())
-      .to.deep.equal(new Vector3(1000000, 10, 0));
-    expect(body2.velocity.round())
-      .to.deep.equal(new Vector3(0, 1, 0));
+    expectVector(body2.getAbsolutePosition(), 1000000, 10, 0);
+    expectVector(body2.position, 1000000, 10, 0);
+    expectVector(body2.velocity, 0, 1, 0);
   });
 
   it('multi-body movement', () => {
@@ -193,16 +165,168 @@ describe('Basic body motion', () => {
     body2.simulate(1, Body.SimulationLevel.NoGravity);
     body3.simulate(1, Body.SimulationLevel.NoGravity);
 
-    expect(body1.position.round()).to.deep.equal(new Vector3(0, 0, 0));
-    expect(body1.velocity.round()).to.deep.equal(new Vector3(0, 0, 0));
-    expect(body1.getAbsolutePosition().round()).to.deep.equal(new Vector3(0, 0, 0));
+    expectVector(body1.position, 0, 0, 0);
+    expectVector(body1.velocity, 0, 0, 0);
+    expectVector(body1.getAbsolutePosition(), 0, 0, 0);
 
-    expect(body2.position.round()).to.deep.equal(new Vector3(1000000, 1, 0));
-    expect(body2.velocity.round()).to.deep.equal(new Vector3(0, 1, 0));
-    expect(body2.getAbsolutePosition().round()).to.deep.equal(new Vector3(1000000, 1, 0));
+    expectVector(body2.position, 1000000, 1, 0);
+    expectVector(body2.velocity, 0, 1, 0);
+    expectVector(body2.getAbsolutePosition(), 1000000, 1, 0);
 
-    expect(body3.position.round()).to.deep.equal(new Vector3(0, 5, 0));
-    expect(body3.velocity.round()).to.deep.equal(new Vector3(0, -5, 0));
-    expect(body3.getAbsolutePosition().round()).to.deep.equal(new Vector3(1000000, 1, 5));
+    expectVector(body3.position, 0, 5, 0);
+    expectVector(body3.velocity, 0, -5, 0);
+    expectVector(body3.getAbsolutePosition(), 1000000, 1, 5);
+  });
+});
+
+describe('Gravitational force', () => {
+  let body1: Body;
+  let body2: Body;
+  let body3: Body;
+
+  beforeEach('Setup bodies', () => {
+    body1 = new Body('1', 0);
+    body1.mass = 1e18;
+
+    body2 = new Body('2', 0);
+    body2.parent = body1;
+    body2.mass = 1e13;
+    body2.position = new Vector3(1000, 0, 0);
+
+    body3 = new Body('3', 0);
+    body3.parent = body2;
+    body3.mass = 1e10;
+    body3.position = new Vector3(10, 0, 0);
+  });
+
+  it('no simulation', () => {
+    expectVector(body1.position, 0, 0, 0);
+    expectVector(body1.velocity, 0, 0, 0);
+    expectVector(body1.getAbsolutePosition(), 0, 0, 0);
+
+    expectVector(body2.position, 1000, 0, 0);
+    expectVector(body2.velocity, 0, 0, 0);
+    expectVector(body2.getAbsolutePosition(), 1000, 0, 0);
+
+    expectVector(body3.position, 10, 0, 0);
+    expectVector(body3.velocity, 0, 0, 0);
+    expectVector(body3.getAbsolutePosition(), 1010, 0, 0);
+  });
+
+  it('no movement', () => {
+    body1.simulate(0, Body.SimulationLevel.TwoBody);
+    body2.simulate(0, Body.SimulationLevel.TwoBody);
+    body3.simulate(0, Body.SimulationLevel.TwoBody);
+
+    expectVector(body1.position, 0, 0, 0);
+    expectVector(body1.velocity, 0, 0, 0);
+    expectVector(body1.getAbsolutePosition(), 0, 0, 0);
+
+    expectVector(body2.position, 1000, 0, 0);
+    expectVector(body2.velocity, 0, 0, 0);
+    expectVector(body2.getAbsolutePosition(), 1000, 0, 0);
+
+    expectVector(body3.position, 10, 0, 0);
+    expectVector(body3.velocity, 0, 0, 0);
+    expectVector(body3.getAbsolutePosition(), 1010, 0, 0);
+  });
+
+  it('no gravity', () => {
+    body1.simulate(1, Body.SimulationLevel.NoGravity);
+    body2.simulate(1, Body.SimulationLevel.NoGravity);
+    body3.simulate(1, Body.SimulationLevel.NoGravity);
+
+    expectVector(body1.position, 0, 0, 0);
+    expectVector(body1.velocity, 0, 0, 0);
+    expectVector(body1.getAbsolutePosition(), 0, 0, 0);
+
+    expectVector(body2.position, 1000, 0, 0);
+    expectVector(body2.velocity, 0, 0, 0);
+    expectVector(body2.getAbsolutePosition(), 1000, 0, 0);
+
+    expectVector(body3.position, 10, 0, 0);
+    expectVector(body3.velocity, 0, 0, 0);
+    expectVector(body3.getAbsolutePosition(), 1010, 0, 0);
+  });
+
+  it('1-step gravity', () => {
+    body1.simulate(1, Body.SimulationLevel.TwoBody);
+    body2.simulate(1, Body.SimulationLevel.TwoBody);
+    body3.simulate(1, Body.SimulationLevel.TwoBody);
+
+    expectVector(body1.position, 0, 0, 0);
+    expectVector(body1.velocity, 0, 0, 0);
+    expectVector(body1.getAbsolutePosition(), 0, 0, 0);
+
+    expectVector(body2.position, 967, 0, 0);
+    expectVector(body2.velocity, -33, 0, 0);
+    expectVector(body2.getAbsolutePosition(), 967, 0, 0);
+
+    expectVector(body3.position, 7, 0, 0);
+    expectVector(body3.velocity, -3, 0, 0);
+    expectVector(body3.getAbsolutePosition(), 973, 0, 0);
+  });
+
+  it('accelerated gravity', () => {
+    body1.simulate(1, Body.SimulationLevel.TwoBody);
+    body2.simulate(1, Body.SimulationLevel.TwoBody);
+    body3.simulate(1, Body.SimulationLevel.TwoBody);
+
+    body1.simulate(2, Body.SimulationLevel.TwoBody);
+    body2.simulate(2, Body.SimulationLevel.TwoBody);
+    body3.simulate(2, Body.SimulationLevel.TwoBody);
+
+    expectVector(body2.position, 898, 0, 0);
+    expectVector(body2.velocity, -69, 0, 0);
+    expectVector(body2.getAbsolutePosition(), 898, 0, 0);
+
+    expectVector(body3.position, -4, 0, 0);
+    expectVector(body3.velocity, -11, 0, 0);
+    expectVector(body3.getAbsolutePosition(), 893, 0, 0);
+  });
+
+  it('Oscillating gravity', () => {
+    body1.simulate(1, Body.SimulationLevel.TwoBody);
+    body2.simulate(1, Body.SimulationLevel.TwoBody);
+    body3.simulate(1, Body.SimulationLevel.TwoBody);
+
+    body1.simulate(2, Body.SimulationLevel.TwoBody);
+    body2.simulate(2, Body.SimulationLevel.TwoBody);
+    body3.simulate(2, Body.SimulationLevel.TwoBody);
+
+    body1.simulate(3, Body.SimulationLevel.TwoBody);
+    body2.simulate(3, Body.SimulationLevel.TwoBody);
+    body3.simulate(3, Body.SimulationLevel.TwoBody);
+
+    expectVector(body2.position, 787, 0, 0);
+    expectVector(body2.velocity, -111, 0, 0);
+    expectVector(body2.getAbsolutePosition(), 787, 0, 0);
+
+    expectVector(body3.position, 4, 0, 0);
+    expectVector(body3.velocity, 8, 0, 0);
+    expectVector(body3.getAbsolutePosition(), 791, 0, 0);
+
+    body1.simulate(4, Body.SimulationLevel.TwoBody);
+    body2.simulate(4, Body.SimulationLevel.TwoBody);
+    body3.simulate(4, Body.SimulationLevel.TwoBody);
+
+    expectVector(body3.position, -9, 0, 0);
+    expectVector(body3.velocity, -13, 0, 0);
+  });
+
+  it('Orbiting', () => {
+    const orbitalDistance = 10;
+    const orbitalSpeed = 5.777;
+    body3.position = new Vector3(orbitalDistance, 0, 0);
+    body3.velocity = new Vector3(0, orbitalSpeed, 0);
+
+    for (let i = 0; i < 100; i++) {
+      for (let j = 0; j < 1000; j++) {
+        body3.simulate(i + (j / 1000), Body.SimulationLevel.TwoBody);
+      }
+      //console.log(`${i+1}s @${body3.position.length().toFixed(1)}m v=[${body3.velocity.length().toFixed(1)}]`);
+      expect(body3.position.length()).to.be.approximately(orbitalDistance, 0.1);
+      expect(body3.velocity.length()).to.be.approximately(orbitalSpeed, 0.1);
+    }
   });
 });
